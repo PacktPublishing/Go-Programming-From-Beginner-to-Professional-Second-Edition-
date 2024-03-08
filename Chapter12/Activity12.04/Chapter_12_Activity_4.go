268465a6a6c207c51325102835217e598ceca37e
Samantha Coyle <sam@diagrid.io>
2024-03-08 08:53:49 -0600

    fix(build): rm binary + go mods
    Signed-off-by: Samantha Coyle <sam@diagrid.io>

--- Chapter12/Activity12.04/Chapter_12_Activity_4.go
+++ Chapter12/Activity12.04/Chapter_12_Activity_4.go
@@ -9,7 +9,7 @@ func main() {
 	current := time.Now()
 	fmt.Println("The current time is:", current.Format(time.ANSIC))
 	// 6 hours, 6 minutes, 6 seconds = 21966
-	SSS := time.Duration(21966 * time.Second)
-	Future := current.Add(SSS)
-	fmt.Println("6 hours, 6 minutes and 6 seconds from now the time will be: ", Future.Format(time.ANSIC))
+	sss := time.Duration(21966 * time.Second)
+	future := current.Add(sss)
+	fmt.Println("6 hours, 6 minutes and 6 seconds from now the time will be: ", future.Format(time.ANSIC))
 }
