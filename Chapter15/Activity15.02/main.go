d6a5faa6c6cc2ab3ab20290cc3d32fd3da74f274
Samantha Coyle <sam@diagrid.io>
2024-03-08 10:20:38 -0600

    style: go fmt files
    Signed-off-by: Samantha Coyle <sam@diagrid.io>

--- Chapter15/Activity15.02/main.go
+++ Chapter15/Activity15.02/main.go
@@ -1,50 +1,52 @@
 package main
 
-import "fmt"
-import "bufio"
-import "os"
-import "strings"
-import "database/sql"
-import _ "github.com/lib/pq"
+import (
+	"bufio"
+	"database/sql"
+	"fmt"
+	"os"
+	"strings"
+
+	_ "github.com/lib/pq"
+)
 
 type Messages struct {
-    UserID int
-    Message string
+	UserID  int
+	Message string
 }
 
-func main(){
+func main() {
 
-var toLookFor string
-var message string
-var email string
-var name string
+	var toLookFor string
+	var message string
+	var email string
+	var name string
 
-reader := bufio.NewReader(os.Stdin)
+	reader := bufio.NewReader(os.Stdin)
 
-messages := []Messages{
-	{1,"Hi Florian, when are you coming home?"},
-	{1,"Can you send some cash?"},
-	{2,"Hi can you bring some bread and milk?"},
-	{7,"Well..."},
-}
+	messages := []Messages{
+		{1, "Hi Florian, when are you coming home?"},
+		{1, "Can you send some cash?"},
+		{2, "Hi can you bring some bread and milk?"},
+		{7, "Well..."},
+	}
 
+	db, err := sql.Open("postgres", "user=postgres password=Start!123 host=127.0.0.1 port=5432 dbname=postgres sslmode=disable")
 
-db, err := sql.Open("postgres", "user=postgres password=Start!123 host=127.0.0.1 port=5432 dbname=postgres sslmode=disable")
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("The connection to the DB was successfully initialized!")
+	}
 
-if err != nil {
-  panic(err)
-}else{
-	fmt.Println("The connection to the DB was successfully initialized!")
-}
+	connectivity := db.Ping()
+	if connectivity != nil {
+		panic(connectivity)
+	} else {
+		fmt.Println("Good to go!")
+	}
 
-connectivity := db.Ping()
-if connectivity != nil{
-	panic(connectivity)
-}else{
-	fmt.Println("Good to go!")
-}
-
-TableCreate := `
+	TableCreate := `
 CREATE TABLE public.messages
 (
     UserID integer NOT NULL,
@@ -59,67 +61,65 @@ ALTER TABLE public.messages
     OWNER to postgres;
 `
 
-_,err = db.Exec(TableCreate)
-if err != nil {
-	panic(err)
-} else{
-	fmt.Println("The table called Messages was successfully created!")
-}
-
-
-insertMessages, insertErr := db.Prepare("INSERT INTO messages VALUES($1,$2)")
-if insertErr != nil{
-	panic(insertErr)
-}
-
-for _, u := range messages{
-	_, err = insertMessages.Exec(u.UserID,u.Message)
-	if err != nil{
+	_, err = db.Exec(TableCreate)
+	if err != nil {
 		panic(err)
-	}else{
-		fmt.Println("The UserID:",u.UserID,"with message:",u.Message,"was successfully added!")
+	} else {
+		fmt.Println("The table called Messages was successfully created!")
 	}
-}
-insertMessages.Close()
 
-fmt.Print("Give me the user's name: ")
+	insertMessages, insertErr := db.Prepare("INSERT INTO messages VALUES($1,$2)")
+	if insertErr != nil {
+		panic(insertErr)
+	}
 
-toLookFor, err = reader.ReadString('\n')
-toLookFor = strings.TrimRight(toLookFor, "\r\n")
-if err != nil{
-	panic(err)
-} else {
-	fmt.Println("Looking for all the messages of user with name:",toLookFor,"##")
-}
+	for _, u := range messages {
+		_, err = insertMessages.Exec(u.UserID, u.Message)
+		if err != nil {
+			panic(err)
+		} else {
+			fmt.Println("The UserID:", u.UserID, "with message:", u.Message, "was successfully added!")
+		}
+	}
+	insertMessages.Close()
 
-UserMessages := "SELECT users.Name, users.Email, messages.Message FROM messages INNER JOIN users ON users.ID=messages.UserID WHERE users.Name LIKE $1"
+	fmt.Print("Give me the user's name: ")
 
-usersMessages, err := db.Prepare(UserMessages)
-if err != nil {
-	panic(err)
-} 
+	toLookFor, err = reader.ReadString('\n')
+	toLookFor = strings.TrimRight(toLookFor, "\r\n")
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("Looking for all the messages of user with name:", toLookFor, "##")
+	}
 
-result, err := usersMessages.Query(toLookFor)
+	UserMessages := "SELECT users.Name, users.Email, messages.Message FROM messages INNER JOIN users ON users.ID=messages.UserID WHERE users.Name LIKE $1"
 
-numberof := 0
-for result.Next(){
-   numberof++
-}
-if numberof == 0 {
-   fmt.Println("The query returned nothing, no such user:",toLookFor)
-}else{
-	fmt.Println("There are a total of",numberof,"messages from the user:",toLookFor)
+	usersMessages, err := db.Prepare(UserMessages)
+	if err != nil {
+		panic(err)
+	}
 
 	result, err := usersMessages.Query(toLookFor)
-	for result.Next(){
-	    err = result.Scan(&name, &email, &message)
-	    if err != nil{
-	    	panic(err)
-	    }
-		fmt.Println("The user:",name,"with email:",email,"has sent the following message:",message)
-	}
-}
-usersMessages.Close()
-db.Close()
-}
 
+	numberof := 0
+	for result.Next() {
+		numberof++
+	}
+	if numberof == 0 {
+		fmt.Println("The query returned nothing, no such user:", toLookFor)
+	} else {
+		fmt.Println("There are a total of", numberof, "messages from the user:", toLookFor)
+
+		result, err := usersMessages.Query(toLookFor)
+		for result.Next() {
+			err = result.Scan(&name, &email, &message)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println("The user:", name, "with email:", email, "has sent the following message:", message)
+		}
+	}
+	usersMessages.Close()
+	db.Close()
+}
