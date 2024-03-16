1e7d82db9b449539e24885a2659dab42349bbf8b
Samantha Coyle <sam@diagrid.io>
2024-03-16 12:58:11 -0500

    fix(ch4): update few things on ch
    Signed-off-by: Samantha Coyle <sam@diagrid.io>

--- Chapter04/Exercise04.20/main.go
+++ Chapter04/Exercise04.20/main.go
@@ -5,12 +5,12 @@ import (
 	"math"
 )
 
-func convert() string{
+func convert() string {
 	var i8 int8 = math.MaxInt8
 	i := 128
 	f64 := 3.14
-	m := fmt.Sprintf("int8    = %v  > in64    = %v\n", i8, int64(i8))
-	m += fmt.Sprintf("int     = %v  > in8     = %v\n", i, int8(i))
+	m := fmt.Sprintf("int8    = %v  > int64    = %v\n", i8, int64(i8))
+	m += fmt.Sprintf("int     = %v  > int8     = %v\n", i, int8(i))
 	m += fmt.Sprintf("int8    = %v  > float32 = %v\n", i8, float64(i8))
 	m += fmt.Sprintf("float64 = %v > int     = %v\n", f64, int(f64))
 	return m
